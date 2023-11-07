172f78342ffe097be35042fcc1925037140f9391
Abdulla-M-Refai <abdullah.m.refai@gmail.com>
2023-11-07 15:02:24 +0200

    fixing bugs and modify the used algorithm now this version is stable and good note this version contains the standard type of nim game only still need to implement at least one other type

--- src/main/java/com/nim/game/util/Helper.java
+++ src/main/java/com/nim/game/util/Helper.java
@@ -4,7 +4,6 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 import javafx.animation.FadeTransition;
